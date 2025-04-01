cb71fb298c4c66eb130c9550d2fbe2fd72565ed1
arwenbae99 <151166379+arwenbae99@users.noreply.github.com>
2025-04-01 14:55:41 +0200

    habe das https entfernt

--- apps/frontend/src/env/env.prod.ts
+++ apps/frontend/src/env/env.prod.ts
@@ -1,7 +1,7 @@
 export const env = {
     production: true,
     api: "https://studio-webux-3cf3a57a6e64.herokuapp.com/api",
-    auth0Domain: 'https://dev-ya68ueq5vs555uca.eu.auth0.com/',
+    auth0Domain: 'dev-ya68ueq5vs555uca.eu.auth0.com',
     auth0ClientId: '5kstur4Ljw3bmPLtEN7aRdrPNVsUJyJW',
     redirectUri: 'https://jfladas.github.io/uxweb2'
     };

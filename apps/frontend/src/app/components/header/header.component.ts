0c89a2d9ea0a5c015bd0834ba1401726d8b03904
LisaBronzeBoogie <47106816+lisa0510@users.noreply.github.com>
2025-04-03 10:52:24 +0200

    sidebar component aufgeteilt

--- apps/frontend/src/app/components/header/header.component.ts
+++ apps/frontend/src/app/components/header/header.component.ts
@@ -2,48 +2,38 @@ import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarComponent],
 })
 export class HeaderComponent {
   @Input() showBackArrow = false;
   @Input() showMenu = false;
 
+  isSidebarOpen = false; // Add this property to control sidebar visibility
+
   private authService = inject(AuthService);
-  isSidebarOpen = false;
 
   constructor(private router: Router) {
     this.authService.isAuthenticated().subscribe((auth) => {
       this.showMenu = auth.authenticated;
     });
   }
-  logout = () => {
-    this.isSidebarOpen = false;
-    this.authService.logout();
-    this.router.navigate(['/login']);
-    this.showMenu = false;
-  };
-
-  onBackArrowClick() {
-    // Handle back arrow click
-  }
-  onMenuClick() {
-    // Handle menu click
-    this.logout();
-  }
-  createEvent() {
-    // Handle create event click
-  }
-
-  startseite() {
-    // Handle startseite click
-  }
 
   toggleSidebar(): void {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
+
+  onBackArrowClick() {
+    // Handle back arrow click
+  }
+
+  onMenuClick() {
+    // Handle menu click
+  }
+
 }

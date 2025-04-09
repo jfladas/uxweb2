b25449216410cc31a3f169c693e748c0dbdf458c
LisaBronzeBoogie <47106816+lisa0510@users.noreply.github.com>
2025-04-09 13:59:29 +0200

    removed and clean up from import

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { FavoritenComponent } from './components/favoriten/favoriten.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent, // Ensure SidebarComponent is declared here
-    FavoritenComponent,
+    SidebarComponent,
   ],
   imports: [BrowserModule],
   providers: [],
